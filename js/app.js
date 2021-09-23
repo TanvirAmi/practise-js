a3a4f4be1bb1bae2e8dd7c9d8d7d847c73e33452
TanvirAmi <tanvir.focus@gmail.com>
2021-09-23 18:01:27 +0600

    Added bind() method

--- js/app.js
+++ js/app.js
@@ -23,6 +23,16 @@ getAge.call(person2);
 function getPersonDetail(occupation, company){
     console.log(this.firstName + ' works as a ' + occupation + ' in ' + company);
 }
-
+//Invokes the function and allows you to pass in arguments as an array
 getPersonDetail.apply(person1, ['Web Dev', 'Theme Junkie']);
 getPersonDetail.apply(person2, ['Executive', 'Square']);
+
+
+//bind() example
+var firstPerson = getPersonDetail.bind(person1);
+//Here we bound the function by creating a copy of getPersonDetail() and now it is able to call 
+//firstPerson() although it wasn't not on the person1 object initailly. it will now recognige it's method and property.
+var firstPerson = getPersonDetail.bind(person1);
+var secondPerson = getPersonDetail.bind(person2);
+firstPerson('Tech Support', 'GfxBucket');
+secondPerson('Executive', 'SKF');
